234e0ae6416b7aa09bfd07786833e61e6b7bbbdb
SamiullahJabbar <samiullahjabbar65@gmail.com>
2026-02-10 15:55:23 +0500

    update backens

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,30 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//     plugins: [react()],
+//     server: {
+//         port: 5173,
+//         proxy: {
+//             '/api': {
+//                 target: 'http://localhost:8001',
+//                 changeOrigin: true,
+//             },
+//         },
+//     },
+// })
+
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 5173,
-        proxy: {
-            '/api': {
-                target: 'http://localhost:8001',
-                changeOrigin: true,
-            },
-        },
-    },
+  plugins: [react()],
+  server: {
+    port: 5173,
+    host: '0.0.0.0', // Isse EC2 par external access asan ho jata hai
+  },
 })
